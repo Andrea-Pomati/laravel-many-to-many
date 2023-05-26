9450e8171dbb7d9263456dadb7ca17f3edec7ab6
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-26 16:49:20 +0200

    fix: {{$project->title

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="main pt-5">
-    <h1 class="">Visualizzazione progetto</h1>
+    <h1>{{$project->title}}</h1>
     <span>Categoria: {{$project->type->name ?? 'nessuna'}}</span>
     <div class="d-flex py-3">
       @foreach($project->technologies as $technology)
@@ -14,34 +14,24 @@
     <hr class="mb-4">
 
     <div class="py-3">
-        <h2 class="text-center">
-            {{$project->title}}
-            <br>
-            <small>
-                <pre class="fs-5">
-                    ({{$project->slug}})
-                </pre>
-            </small>
-        </h2>  
-    </div>
-
-    <hr>
 
     <p>
         {{$project->content}}
     </p>
 
+  </div>
+
+
+
+    <div class="d-flex justify-content-around">
+      <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary">Modifica il progetto</a>
+        <!-- Button trigger modal -->
+      <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Elimina
+      </button>
+    </div>
+
 </div>
-
-<div class="d-flex justify-content-around">
-    <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary">Modifica il progetto</a>
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Elimina
-  </button>
-</div>
-
-
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
